2e23ad951cef4e18825c0e834d39f3ccd1aef0c4
Pablo Lagger <29312124+LaggerP@users.noreply.github.com>
2024-11-06 20:28:50 -0300

    Drop cost
    se agrega una variable de tipo export determinando el valor del costo de un drop

--- objects/drop.gd
+++ objects/drop.gd
@@ -1,13 +1,16 @@
 extends Node3D
 class_name Drop
 
+## Este valor determina el costo de este drop.
+## Mientras mas alto sea el valor, mas tiempo se le dara al jugador para poder agarrarlo.
+@export var cost: int = 0
+
 func _on_drops_player_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
 		$Label3D.text = "OK"
 		for d in $drops.get_children():
 			if d.is_class("RigidBody3D"):
 				d.queue_free()
-		
 		await get_tree().create_timer(3).timeout
 		queue_free()
 	pass
