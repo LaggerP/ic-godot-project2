69578ae0dfc3f7dcc88523f92d3faa4bd2fe1eeb
Pablo Lagger <29312124+LaggerP@users.noreply.github.com>
2024-12-07 12:45:34 -0300

    solved double drop
    se soluciono el bug de que un jugador puede obtener dos veces el mismo objeto si este gira

--- scenes/objects/drops/drop.gd
+++ scenes/objects/drops/drop.gd
@@ -1,8 +1,11 @@
 extends Node3D
 class_name Drop
 
+var is_taken: bool = false
+
 func _on_drops_player_entered(body: Node3D) -> void:
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") and not is_taken:
+		is_taken = true
 		$drops/DropCollision.disabled
 		$Label3D.text = "OK"
 		GameManager.obtain_drop(self)
